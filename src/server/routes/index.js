7117a3ce960eca6109e7fb5e02b96a46ddc1904e
millsmcilroy <millsmcilroy@gmail.com>
2016-03-02 14:05:27 -0700

    added sort functionality

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,8 +8,74 @@ var pgp = require('pg-promise')(options);
 var connectionString = 'postgres://localhost:5432/movie_inventory_crud';
 var db = pgp(connectionString);
 
+//Keeps track of index page movie display
+var titleClicked = false;
+var yearClicked = false;
+var date_obtainedClicked = false;
+var ratingClicked = false;
+var typeClicked = false;
+
 //Returns all movies with basic info
 router.get('/', function(req, res, next) {
+  console.log(req.query);
+  if (req.query.title) {
+    db.any('SELECT * FROM movies ORDER BY title ' + req.query.title)
+    .then(function (data) {
+      res.status(200).render('index', { data:data, title:req.query.title });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+  else if (req.query.year) {
+    db.any('SELECT * FROM movies ORDER BY year ' + req.query.year)
+    .then(function (data) {
+      res.status(200).render('index', { data:data, year:req.query.year});
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+  else if (req.query.date_obtained) {
+    db.any('SELECT * FROM movies ORDER BY date_obtained ' + req.query.date_obtained)
+    .then(function (data) {
+      res.status(200).render('index', { data:data, date_obtained:req.query.date_obtained });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+  else if (req.query.rating) {
+    db.any('SELECT * FROM movies ORDER BY rating ' + req.query.rating)
+    .then(function (data) {
+      res.status(200).render('index', { data:data, rating:req.query.rating });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+  else if (req.query.type) {
+    db.any('SELECT * FROM movies ORDER BY type ' + req.query.type)
+    .then(function (data) {
+      res.status(200).render('index', { data:data, type:req.query.type });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  }
+  else {
+    db.any('SELECT * FROM movies ORDER BY date_obtained DESC')//Returns an array
+      .then(function (data) {//Name the array var "data"
+        res.status(200).render('index', { data:data, date_obtained:'desc' });
+      })
+      .catch(function (err) {
+        return next(err);
+      });
+  }
+});
+
+/*/Returns all movies with basic info
+router.get('/?[column]=[asc/desc]', function(req, res, next) {
   db.any('SELECT * FROM movies ORDER BY date_obtained')//Returns an array
     .then(function (data) {//Name the array var "data"
       res.status(200).render('index', { data:data });
@@ -17,7 +83,7 @@ router.get('/', function(req, res, next) {
     .catch(function (err) {
       return next(err);
     });
-});
+});*/
 
 //Goes to new movie page
 router.get('/movies/new', function(req, res, next) {
@@ -39,7 +105,10 @@ router.get('/movies/:id/edit', function(req, res, next) {
 
 //Creates a new movie in database
 router.post('/movies', function(req, res, next) {
-  db.any("INSERT INTO movies (title,description,image_url,year,date_obtained,rating,notes,type) VALUES ('" + req.body.title + "','" + req.body.description + "','" + req.body.imageUrl + "'," + req.body.year + ", '2016-03-01' ," + req.body.rating + ",'" + req.body.notes + "','" + req.body.type + "');")
+  var thisDay = formatDate();
+  console.log(thisDay);
+  console.log("INSERT INTO movies (title,description,image_url,year,date_obtained,rating,notes,type) VALUES ('" + req.body.title + "','" + req.body.description + "','" + req.body.imageUrl + "'," + req.body.year + ",'" + thisDay + "'," + req.body.rating + ",'" + req.body.notes + "','" + req.body.type + "');");
+  db.any("INSERT INTO movies (title,description,image_url,year,date_obtained,rating,notes,type) VALUES ('" + req.body.title + "','" + req.body.description + "','" + req.body.imageUrl + "'," + req.body.year + ",'" + thisDay + "'," + req.body.rating + ",'" + req.body.notes + "','" + req.body.type + "');")
     .then(function () {
       res.status(200).redirect('/');
     })
@@ -71,14 +140,16 @@ router.post('/movies/:id/delete', function(req, res, next) {
     });
 });
 
-function formatDate(date) {
-  var d = new Date(date),
+//Returns the current date in sql friendly string format to be
+//used by the add new movie page for date_obtained
+var formatDate = function () {
+  var d = new Date(),
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
       year = d.getFullYear();
   if (month.length < 2) month = '0' + month;
   if (day.length < 2) day = '0' + day;
-  return [month, day, year].join('-');
-}
+  return year + month + day;
+};
 
 module.exports = router;
